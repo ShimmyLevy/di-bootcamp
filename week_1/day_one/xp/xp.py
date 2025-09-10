aedc2849f1896c95576e216a4839fb0de3908681
shimmy <shimon.refoel.levy@gmail.com>
2025-09-10 15:53:51 +0300

    adding xp day one

---
+++ week_1/day_one/xp/xp.py
@@ -0,0 +1,62 @@
+# week one \ day one \ XP
+
+#exercise 1
+print('hello world\n'*4)
+
+#exercise 2
+print ((99^3)*8)
+
+#exercise 3
+print (5 < 3) #false
+print (3 == 3) #true
+print (3 == "3") #false
+print ("3" > 3) #error
+print ("Hello" == "hello") #false
+
+#exercise 4
+computer_brand = "lenovo thinkpad"
+print("i have a " + computer_brand + " computer")
+
+#exercise 5
+name = ("shimmy")
+age = ("24")
+shoe_size = ("47")
+info = ('my name is ' + name + ' i am ' + age + ' years old and im tall with big feet, my shoe size is ' + shoe_size)
+print(info)
+
+#exercise 6
+A = 17
+B = 14
+
+if (A > B):
+    print("Hello world")
+
+else:
+    print("woops")
+
+#exercise 7
+
+number = int(input('pls pick a number')) 
+if number % 2==0:
+    print(f"{number} is an even number")
+
+else:
+    print(f"{number} is an odd number")
+
+exercise 8
+your_name = input("Hi, my name is shimmy! \nwhats your name?")
+
+if your_name == "shimmy":
+    print("no silly thats my name")
+
+else:
+    print("haha thats my dogs name")
+
+#exercise 9
+hight = int(input("whats you hight in cm?"))
+
+if hight >= 145:
+    print("you are " f"{hight}" "cm tall which is above the 145cm requirement to access the ride")
+
+elif hight < 145:
+    print("sorry you are " f"{hight}" "cm tall which is below the 145cm requirment to ride you will need to grow some more,\n better luck next time!")
