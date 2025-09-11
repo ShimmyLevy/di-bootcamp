058a5180a227bf2f4daa02e5b8c1f4cd0cf0e510
shimmy <shimon.refoel.levy@gmail.com>
2025-09-11 11:45:32 +0300

    submitting gold XP

--- week_1/day_one/xp/xp.py
+++ week_1/day_one/xp/xp.py
@@ -1,62 +1,70 @@
-# week one \ day one \ XP
+# # week one \ day one \ XP
 
-#exercise 1
-print('hello world\n'*4)
+# #exercise 1
+# print('hello world\n'*4)
 
-#exercise 2
-print ((99^3)*8)
 
-#exercise 3
-print (5 < 3) #false
-print (3 == 3) #true
-print (3 == "3") #false
-print ("3" > 3) #error
-print ("Hello" == "hello") #false
+# #exercise 2
+# print ((99**3)*8)
 
-#exercise 4
-computer_brand = "lenovo thinkpad"
-print("i have a " + computer_brand + " computer")
 
-#exercise 5
-name = ("shimmy")
-age = ("24")
-shoe_size = ("47")
-info = ('my name is ' + name + ' i am ' + age + ' years old and im tall with big feet, my shoe size is ' + shoe_size)
-print(info)
+# #exercise 3
+# print (5 < 3) #false
+# print (3 == 3) #true
+# print (3 == "3") #false
+# print ("3" > 3) #error because its comparing a string to an int
+# print ("Hello" == "hello") #false
 
-#exercise 6
-A = 17
-B = 14
 
-if (A > B):
-    print("Hello world")
+# #exercise 4
+# computer_brand = "lenovo thinkpad"
+# print("i have a " + computer_brand + " computer")
 
-else:
-    print("woops")
 
-#exercise 7
+# #exercise 5
+# name = ("shimmy")
+# age = ("24")
+# shoe_size = ("47")
+# info = ('my name is ' + name + ' i am ' + age + ' years old and im tall with big feet, my shoe size is ' + shoe_size)
+# print(info)
 
-number = int(input('pls pick a number')) 
-if number % 2==0:
-    print(f"{number} is an even number")
 
-else:
-    print(f"{number} is an odd number")
+# #exercise 6
+# A = 17
+# B = 14
 
-exercise 8
-your_name = input("Hi, my name is shimmy! \nwhats your name?")
+# if (A > B):
+#     print("Hello world")
 
-if your_name == "shimmy":
-    print("no silly thats my name")
+# else:
+#     print("woops")
 
-else:
-    print("haha thats my dogs name")
 
-#exercise 9
-hight = int(input("whats you hight in cm?"))
+# #exercise 7
 
-if hight >= 145:
-    print("you are " f"{hight}" "cm tall which is above the 145cm requirement to access the ride")
+# number = int(input('pls pick a number')) 
+# if number % 2==0:
+#     print(f"{number} is an even number")
 
-elif hight < 145:
-    print("sorry you are " f"{hight}" "cm tall which is below the 145cm requirment to ride you will need to grow some more,\n better luck next time!")
+# else:
+#     print(f"{number} is an odd number")
+
+
+# #exercise 8
+# your_name = input("Hi, my name is shimmy! \nwhats your name?")
+
+# if your_name == "shimmy":
+#     print("no silly thats my name")
+
+# else:
+#     print("haha thats my dogs name")
+
+
+# #exercise 9
+# hight = int(input("whats you hight in cm?"))
+
+# if hight >= 145:
+#     print("you are " f"{hight}" "cm tall which is above the 145cm requirement to access the ride")
+
+# elif hight < 145:
+#     print("sorry you are " f"{hight}" "cm tall which is below the 145cm requirment to ride you will need to grow some more,\n better luck next time!")
---
+++ week_1/day_one/xp/xp_gold.py
@@ -0,0 +1,22 @@
+# # week one \ day one \ Excercises XP Gold
+
+
+# # Excercise 1
+ex1 = "Hello world\n"
+ex2 = "I love python\n"
+print(ex1*4 + ex2*4)
+
+
+# # Excercise 2
+birthday = input("Which month were you born in? ")
+if birthday in ("march", "april", "may"):
+    print("you were born in spring")
+
+elif birthday in ("june", "july", "august"):
+    print("you are a sunshine summer baby")
+
+elif birthday in ("september", "october", "november"):
+    print("you were born in the colorful autumn")
+
+else:
+    print("you were born in the cold winter")
